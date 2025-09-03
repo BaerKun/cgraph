6fa620abb86e7956053ba932a5b0019d28dd2311
Bear Kun <bear_kun@outlook.com>
2025-09-03 20:22:12 +0800

    [delete] random/any_colony

--- src/alg/ant_colony.c
+++
@@ -1,7 +0,0 @@
-#include "cgraph/linked_list.h"
-#include "private/graph_detail.h"
-#include "private/random.h"
-#include <stdlib.h>
-#include <time.h>
-
-void antColony(CGraph *graph, WeightType weight[]) {}
